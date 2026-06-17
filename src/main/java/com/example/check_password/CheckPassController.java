75c85d1c450e51a5d1d90deb9658355f0da58de0
leeyoungkon <yklee2002@gmail.com>
2026-06-18 00:41:42 +0900

    TA-3 make view

--- src/main/java/com/example/check_password/CheckPassController.java
+++ src/main/java/com/example/check_password/CheckPassController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class CheckPassController {
+public class CheckPassController {  
 
 	private static final String VALID_PASSWORD = "1234";
 
